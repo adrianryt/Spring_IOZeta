91c007fb2719438ff1d847ba97e6a5aae48787fc
Kuba <kuba.nowakowski@sabre.com>
2022-03-29 18:58:08 +0200

    init

---
+++ src/main/java/com/iozeta/spring_iozeta/SpringIoZetaApplication.java
@@ -0,0 +1,13 @@
+package com.iozeta.spring_iozeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIoZetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIoZetaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iozeta/spring_iozeta/SpringIoZetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iozeta.spring_iozeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIoZetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
