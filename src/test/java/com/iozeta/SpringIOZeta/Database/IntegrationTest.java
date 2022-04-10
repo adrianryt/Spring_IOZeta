77b1892ba4e46d127641047183f3973c1f21a827
war-in <marcin.warchol20@gmail.com>
2022-04-10 13:20:52 +0200

    modified test

--- src/test/java/com/iozeta/SpringIOZeta/Database/IntegrationTest.java
+++ src/test/java/com/iozeta/SpringIOZeta/Database/IntegrationTest.java
@@ -34,101 +34,129 @@ public class IntegrationTest {
 
     @Test
     public void entireFlow() {
+
+        //create lecturer with some parameters
         Lecturer lecturer = new Lecturer();
         lecturer.setName("Lecturer");
         lecturer.setSurname("LSurname");
         lecturer.setGitNick("LSNick");
         lecturer.setGitToken("gitToken1");
-        lecturerRepository.save(lecturer);
+        Lecturer tmpLecturer = lecturerRepository.save(lecturer);
 
-        lecturer = lecturerRepository.getById(1L);
+        //get lecturer from database
+        lecturer = lecturerRepository.getById(tmpLecturer.getId());
 
+        //create subject
         Subject subject = new Subject();
         subject.setName("IO");
         subject.setLecturer(lecturer);
         subject.setRepoName("IORepo");
-        subjectRepository.save(subject);
+        Subject tmpSubject = subjectRepository.save(subject);
 
+        //add subject to lecturer and save
         lecturer.addSubject(subject);
         lecturerRepository.save(lecturer);
 
-        subject = subjectRepository.getById(1L);
+        //get subject from database
+        subject = subjectRepository.getById(tmpSubject.getId());
 
+        //create task
         Task task = new Task();
         task.setName("Task1");
         task.setSubject(subject);
         task.setBranchName("IO_Task1");
-        taskRepository.save(task);
+        Task tmpTask = taskRepository.save(task);
 
+        //add task to subject and save
         subject.addTask(task);
         subjectRepository.save(subject);
 
-        task = taskRepository.getById(1L);
+        //get task from database
+        task = taskRepository.getById(tmpTask.getId());
 
+        //create checkpoint
         Checkpoint checkpoint = new Checkpoint();
         checkpoint.setNumber(1);
         checkpoint.setContent("Checkpoint1: content");
         checkpoint.setTask(task);
-        checkpointRepository.save(checkpoint);
+        Checkpoint tmpCheckpoint = checkpointRepository.save(checkpoint);
 
+        //create session
         Session session = new Session();
         session.setName("Name");
         session.setAccess_code("Some code");
         session.setTask(task);
         session.setActive(true);
-        sessionRepository.save(session);
+        Session tmpSession = sessionRepository.save(session);
 
+        //add checkpoint to task
         task.addCheckpoint(checkpoint);
+        //add session to task
         task.addSession(session);
 
+        //create student
         Student student = new Student();
         student.setGitNick("Git nick");
         student.setBranchName("Branch name");
-        studentRepository.save(student);
+        Student tmpStudent = studentRepository.save(student);
 
-        student = studentRepository.getById(1L);
-        session = sessionRepository.getById(1L);
-        checkpoint = checkpointRepository.getById(1L);
+        //get student, session and checkpoint from database
+        student = studentRepository.getById(tmpStudent.getId());
+        session = sessionRepository.getById(tmpSession.getId());
+        checkpoint = checkpointRepository.getById(tmpCheckpoint.getId());
 
+        //create progress
         Progress progress = new Progress();
         progress.setSession(session);
         progress.setStudent(student);
         progress.setCheckpoint(checkpoint);
         progress.setDone(false);
-        progressRepository.save(progress);
+        Progress tmpProgress = progressRepository.save(progress);
 
+        //add progress to student, session and checkpoint
         student.addProgress(progress);
         session.addProgress(progress);
         checkpoint.addProgress(progress);
 
+        //save changes
+        studentRepository.save(student);
+        sessionRepository.save(session);
+        checkpointRepository.save(checkpoint);
 
-        Lecturer lecturer2 = lecturerRepository.getById(1L);
-        Subject subject2 = subjectRepository.getById(1L);
-        Task task2 = taskRepository.getById(1L);
-        Checkpoint checkpoint2 = checkpointRepository.getById(1L);
-        Session session2 = sessionRepository.getById(1L);
-        Progress progress2 = progressRepository.getById(1L);
-        Student student2 = studentRepository.getById(1L);
+        //get all from database
+        Lecturer lecturer2 = lecturerRepository.getById(tmpLecturer.getId());
+        Subject subject2 = subjectRepository.getById(tmpSubject.getId());
+        Task task2 = taskRepository.getById(tmpCheckpoint.getId());
+        Checkpoint checkpoint2 = checkpointRepository.getById(tmpCheckpoint.getId());
+        Session session2 = sessionRepository.getById(tmpSession.getId());
+        Progress progress2 = progressRepository.getById(tmpProgress.getId());
+        Student student2 = studentRepository.getById(tmpStudent.getId());
 
-
+        //check relation between lecturer and subject
         Assertions.assertTrue(lecturer2.getSubjects().contains(subject2));
         Assertions.assertEquals(subject2.getLecturer(), lecturer2);
 
+        //check relation between subject and task
         Assertions.assertTrue(subject2.getTasks().contains(task2));
         Assertions.assertEquals(task2.getSubject(), subject2);
 
+        //check relation between task and checkpoint
         Assertions.assertTrue(task2.getCheckpoints().contains(checkpoint2));
         Assertions.assertEquals(checkpoint2.getTask(), task2);
 
+        //check relation between task and session
         Assertions.assertTrue(task2.getSessions().contains(session2));
         Assertions.assertEquals(session2.getTask(), task2);
 
+        //check relation between session and progress
         Assertions.assertTrue(session2.getProgresses().contains(progress2));
         Assertions.assertEquals(progress2.getSession(), session2);
 
+        //check relation between checkpoint and progress
         Assertions.assertTrue(checkpoint2.getProgresses().contains(progress2));
         Assertions.assertEquals(progress2.getCheckpoint(), checkpoint2);
 
+        //check relation between student and progress
         Assertions.assertTrue(student2.getProgresses().contains(progress2));
         Assertions.assertEquals(progress2.getStudent(), student2);
     }
