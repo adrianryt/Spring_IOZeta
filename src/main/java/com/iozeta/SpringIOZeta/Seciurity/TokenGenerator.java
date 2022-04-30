dccec83b209234321d126afaa455b29ec0c424e7
Kuba <kuba.nowakowski@sabre.com>
2022-04-30 23:24:19 +0200

    token autoryzacyjny na godzinę i odświerzający na 24h

--- src/main/java/com/iozeta/SpringIOZeta/Seciurity/TokenGenerator.java
+++ src/main/java/com/iozeta/SpringIOZeta/Seciurity/TokenGenerator.java
@@ -2,7 +2,6 @@ package com.iozeta.SpringIOZeta.Seciurity;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-
 import com.iozeta.SpringIOZeta.Database.Entities.Lecturer;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
@@ -17,9 +16,9 @@ public class TokenGenerator {
     @Getter
     private static final Algorithm algorithm = Algorithm.HMAC256("secret".getBytes(StandardCharsets.UTF_8));
     @Getter
-    private static final long accessTokenTimeToLive = 10 * 60 * 1000;
+    private static final long accessTokenTimeToLive = 60 * 60 * 1000;
     @Getter
-    private static final long refreshTokenTimeToLive = 60 * 60 * 1000;
+    private static final long refreshTokenTimeToLive = 24 * 60 * 60 * 1000;
 
     public static String generateAccessToken(Lecturer user, HttpServletRequest request) {
         return JWT.create()
