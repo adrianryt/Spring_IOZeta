0257b235fe78ab654a26de3ce83708614b5690c4
Krzysztof Gądek <305482@student.agh.edu.pl>
2022-05-17 17:19:54 +0200

    little fixes by Grzychu

--- src/main/java/com/iozeta/SpringIOZeta/SecurityConfig/filter/SimpleCORSFilter.java
+++ src/main/java/com/iozeta/SpringIOZeta/SecurityConfig/filter/SimpleCORSFilter.java
@@ -13,9 +13,9 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, authorization");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
