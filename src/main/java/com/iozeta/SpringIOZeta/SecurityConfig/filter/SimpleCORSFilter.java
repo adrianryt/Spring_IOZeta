fe0bc610c78022f8797859a1dd875785492ce4ab
adrianryt <adrianryt3@gmail.com>
2022-06-05 19:41:53 +0200

    Change CORS filter

--- src/main/java/com/iozeta/SpringIOZeta/SecurityConfig/filter/SimpleCORSFilter.java
+++ src/main/java/com/iozeta/SpringIOZeta/SecurityConfig/filter/SimpleCORSFilter.java
@@ -12,7 +12,7 @@ public class SimpleCORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "https://io-frontend-demo.herokuapp.com");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, authorization");
